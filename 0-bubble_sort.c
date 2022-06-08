0a089849b11c1180189c4cdecfc3beec302fd074
Dak4rPrO <98028958+Dak4rPrO@users.noreply.github.com>
2022-06-08 18:25:32 -0300

    no anda visual :c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+/**
+ * @brief
+ * 
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+    i = 0;
+    j = i + 1;
+
+    for (; i < size; i++);
+    {
+        for (; j <= size; j++)
+        {
+            if (array[i] > array[j])
+            {
+                swap = array[i]
+                array[j] = array[i]
+                array[i] = swap
+            }
+            print_array(array, size);
+        }
+        
+    }
+}
