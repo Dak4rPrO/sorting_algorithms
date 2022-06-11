1ee8ef3bd508b013d501d9cecdf12c8a18a18433
Dak4rPrO <4382@holbertonschool.com>
2022-06-11 13:50:30 -0700

    0-O salto de linea al final

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,19 +9,19 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j, swap;
+	size_t i, j, swap;
 
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                swap = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = swap;
-		print_array(array, size);
-            }
-        }
-    }
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = swap;
+				print_array(array, size);
+			}
+		}
+	}
 }
