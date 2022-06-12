752b9e6c914f44f84c8094811948bc7e931799a1
Dak4rPrO <4382@holbertonschool.com>
2022-06-11 20:45:40 -0700

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,30 +1,38 @@
 #include "sort.h"
 
 /**
- * bubble_sort - function that sorts an array of integers in ascending order
- * using the Bubble sort algorithm.
+ * selection_sort - function that sorts an array of integers
+ * in ascending order using the Selection sort algorithm.
  * @array: array con los elementos (int) a ordenar.
  * @size: el tamaño del array.
+ * i: recorremos el array.
+ * j: variable a comparar con el dato (int) mas pequeño del array.
+ * h: variable que busca el dato mas pequeño de todo el array.
+ * swap: variable que almacena una posicion, para luego ser remplazada.
  */
 
-void bubble_sort(int *array, size_t size)
+void selection_sort(int *array, size_t size)
 {
-        size_t i, j, swap;
+	size_t i, j, h, swap;
 
-        if (array == NULL)
-                return;
+	if (array == NULL)
+		return;
 
-        for (i = 0; i < size; i++)
-        {
-                for (j = 0; j < size - 1; j++)
-                {
-                        if (array[j] > array[j + 1])
-                        {
-                                swap = array[j];
-                                array[j] = array[j + 1];
-                                array[j + 1] = swap;
-                                print_array(array, size);
-                        }
-                }
-        }
+	for (i = 0; i < size; i++)
+	{
+		h = i;
+		j = i + 1;
+
+		for (; j < size; j++)
+		{
+			if (array[j] < array[h])
+				h = j;
+
+			swap = array[i];
+			array[i] = array[h];
+			array[h] = swap;
+		}
+		if (h != i)
+			print_array(array, size);
+	}
 }
